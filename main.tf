7259122acd5560746ed83e288de76186cfa3804d
kodalikranthi <48436726+kodalikranthi@users.noreply.github.com>
2019-07-05 00:19:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-   # access_key = "${var.aws_access_key}"
-   # secret_key = "${var.aws_secret_key}"
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
     region = "${var.aws_region}"
 profile="default"
 }
@@ -95,7 +95,7 @@ resource "aws_instance" "kranthiweb" {
     ami = "ami-0080e4c5bc078760e"
     availability_zone = "us-east-1a"
     instance_type = "t2.micro"
-    key_name = "Regular Key"
+    key_name = "bhavya"
     subnet_id = "${aws_subnet.subnet1-public.id}"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
     associate_public_ip_address = true	
