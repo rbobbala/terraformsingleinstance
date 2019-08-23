1f175954e5e0ab2b67bbf8da65c7b9f14cce44fd
kodalikranthi <48436726+kodalikranthi@users.noreply.github.com>
2019-08-23 12:49:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ resource "aws_instance" "kranthiweb" {
     ami = "ami-0080e4c5bc078760e"
     availability_zone = "us-east-1a"
     instance_type = "t2.micro"
-    key_name = "bhavya"
+    key_name = "kranthi1"
     subnet_id = "${aws_subnet.subnet1-public.id}"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
     associate_public_ip_address = true	
