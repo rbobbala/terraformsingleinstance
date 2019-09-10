80a45cc6515c1ee29119ee6b56e662a0beb24368
rbobbala <brbabu6086@gmail.com>
2019-09-10 12:25:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ resource "aws_instance" "kranthiweb" {
     ami = "ami-0080e4c5bc078760e"
     availability_zone = "us-east-1a"
     instance_type = "t2.micro"
-    key_name = "kranthi1"
+    key_name = "Ravi_jenkins"
     subnet_id = "${aws_subnet.subnet1-public.id}"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
     associate_public_ip_address = true	
@@ -103,7 +103,7 @@ resource "aws_instance" "kranthiweb" {
     tags = {
       Name = "Server-TF"
       Env = "Prod"
-       Owner = "kranthi"
+       Owner = "Ravindra"
     }
 }
 
