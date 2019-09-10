0f00267dce756615a2019cb13b195a6491202bbc
rbobbala <brbabu6086@gmail.com>
2019-09-10 14:38:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ resource "aws_instance" "kranthiweb" {
     ami = "ami-0080e4c5bc078760e"
     availability_zone = "us-east-1a"
     instance_type = "t2.micro"
-    key_name = "Ravi_Jenkins"
+    key_name = "Terraform_POC"
     subnet_id = "${aws_subnet.subnet1-public.id}"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
     associate_public_ip_address = true	
